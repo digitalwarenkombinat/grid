ad41ba2fffe9009ae7b9d06012d36cc49be76de1
Gerd Müller <gerdesque@posteo.org>
2024-10-15 19:22:51 +0200

    🔨 refactor(app): improve strict TypeScript typing

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 # Grid
 
-Inspired from [Creating Generative SVG Grids](https://frontend.horse/articles/generative-grids/)
-Written by Alex Trost
+
+
+
+# Grid Art Designer
+
+## Overview
+Grid Art Designer allows users to create and download grid-based SVG artwork, with options to toggle filters and download the final image as an SVG or PNG.
+
+Inspired from Alex Trosts article [Creating Generative SVG Grids](https://frontend.horse/articles/generative-grids/).
+
+## Installation
+
+1. Clone the repository:
+
+```
+  git clone https://github.com/your-repo/grid-art-designer.git
+  cd grid-art-designer
+```
+
+2. Install dependencies:
+
+```
+  npm install
+```
+
+3. Run the development server:
+
+```
+  npm run dev
+```
+
+4. Open http://localhost:3000 to view the app.
+
+## Usage
+
+- Use the filters to apply different block styles.
+- Download your artwork by clicking "Download SVG" or "Download PNG".
+
+## Dependencies
+
+- Next.js: React framework for server-side rendering.
+- React: JavaScript library for building user interfaces.
+- Tailwind CSS: Utility-first CSS framework.
+- html-to-image: Library for converting HTML nodes to images.
+
+## License
+
+This project is licensed under the MIT License.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,59 @@
 /* eslint-disable @next/next/no-img-element */
 import { random } from "@georgedoescode/generative-utils";
 import { Svg, SVG } from "@svgdotjs/svg.js";
-import { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import Chip from '@mui/material/Chip';
-import Container from '@mui/material/Container';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
+import { useState, useRef } from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import Container from "@mui/material/Container";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
 import gsap from "gsap";
-import Head from 'next/head'
-import Stack from '@mui/material/Stack';
-import Switch from '@mui/material/Switch';
+import Head from "next/head";
+import Stack from "@mui/material/Stack";
+import Switch from "@mui/material/Switch";
 import tinycolor from "tinycolor2";
-import Toolbar from '@mui/material/Toolbar';
-import type { NextPage } from 'next'
-import Typography from '@mui/material/Typography';
+import Toolbar from "@mui/material/Toolbar";
+import type { NextPage } from "next";
+import Typography from "@mui/material/Typography";
+import { toPng } from "html-to-image";
 
-import { drawCross, drawDots, drawHalfSquare, drawDiagonalSquare, drawCircle, drawOppositeCircles, drawQuarterCircle, drawLetterBlock } from "../src/blockDesign";
+import {
+  drawCross,
+  drawDots,
+  drawHalfSquare,
+  drawDiagonalSquare,
+  drawCircle,
+  drawOppositeCircles,
+  drawQuarterCircle,
+  drawLetterBlock,
+} from "../src/blockDesign";
 
+// Define variables with strict types
+export let draw: Svg;
+export let squareSize: number;
+export let numRows: number;
+export let numCols: number;
+export let colors: string[];
+export let colorPalette: string[];
 
-export let draw: Svg, squareSize: number, numRows: number, numCols: number, colors: string[], colorPalette: string[];
+type BlockStyleFunction = (
+  xPos: number,
+  yPos: number,
+  foreground: string,
+  background: string,
+  diwakoStyle: boolean
+) => void;
 
 /*
 Utility Functions
 */
 
+// Generates a small block
 function generateLittleBlock(i: number, j: number, diwakoStyle: boolean) {
   const { foreground, background } = getTwoColors(colorPalette);
 
-  const blockStyleOptions = [
+  const blockStyleOptions: BlockStyleFunction[] = [
     drawCross,
     drawDots,
     drawHalfSquare,
@@ -37,7 +61,7 @@ function generateLittleBlock(i: number, j: number, diwakoStyle: boolean) {
     drawCircle,
     drawOppositeCircles,
     drawQuarterCircle,
-    drawLetterBlock
+    drawLetterBlock,
   ];
 
   const blockStyle = random(blockStyleOptions);
@@ -47,58 +71,51 @@ function generateLittleBlock(i: number, j: number, diwakoStyle: boolean) {
   blockStyle(xPos, yPos, foreground, background, diwakoStyle);
 }
 
+// Generates a large block
 function generateBigBlock(diwakoStyle: boolean) {
   const { foreground, background } = getTwoColors(colorPalette);
 
-  const blockStyleOptions = [
+  const blockStyleOptions: BlockStyleFunction[] = [
     drawCross,
     drawHalfSquare,
     drawDiagonalSquare,
     drawCircle,
     drawQuarterCircle,
     drawOppositeCircles,
-    drawLetterBlock
+    drawLetterBlock,
   ];
 
-  let prevSquareSize = squareSize;
+  const prevSquareSize = squareSize;
 
-  // Random multiplier (2 or 3 squares)
   const multiplier = random([2, 3]);
-  // Random X position
   const xPos = random(0, numRows - multiplier, true) * prevSquareSize;
-  // Random Y position
   const yPos = random(0, numCols - multiplier, true) * prevSquareSize;
 
-  // Make squareSize bigger
   squareSize = multiplier * 100;
 
-  // Get random square style
   const blockStyle = random(blockStyleOptions);
   blockStyle(xPos, yPos, foreground, background, diwakoStyle);
 
-  // Reset squareSize
   squareSize = prevSquareSize;
 }
 
-function getTwoColors(colors: string[]) {
-  let colorList = [...colors];
-  // Get random index for this array of colors
+// Utility function to get two random colors
+function getTwoColors(colors: string[]): { foreground: string; background: string } {
+  const colorList = [...colors];
   const colorIndex = random(0, colorList.length - 1, true);
-  // Set the background to the color at that array
   const background = colorList[colorIndex];
-  // Remove that color from the options
   colorList.splice(colorIndex, 1);
-  // Set the foreground to any other color in the array
   const foreground = random(colorList);
 
   return { foreground, background };
 }
 
 const Home: NextPage = () => {
+  const [colors, setColors] = useState<string[]>([]);
+  const [diwakoStyle, setDiwakoStyle] = useState<boolean>(false);
+  const [init, setInit] = useState<boolean>(false);
 
-  const [colors, setColors] = useState([''])
-  const [diwakoStyle, setDiwakoStyle] = useState(false)
-  const [init, setInit] = useState(false)
+  const svgContainerRef = useRef<HTMLDivElement>(null);
 
   /*
   Create New Piece
@@ -106,71 +123,64 @@ const Home: NextPage = () => {
 
   function generateNewGrid() {
     setInit(true);
-    // Fade out SVG
     gsap.to(".container > svg", {
       opacity: 0,
       scale: 0.8,
       duration: 0.25,
       onComplete: () => {
-        // Remove previous SVG from DOM
-        let container = document.querySelector(".container")
-        if (container){ 
+        const container = document.querySelector(".container");
+        if (container) {
           container.innerHTML = "";
         }
-        // Start new SVG creation
         drawGrid();
-      }
+      },
     });
   }
 
   async function drawGrid() {
-    // Get color palettes
-    const response = await fetch('https://unpkg.com/nice-color-palettes@3.0.0/100.json');
-    const colors = await response.json();
-  
-    // Set Random Palette
-    colorPalette = diwakoStyle ? ['#dd8d0e', '#009599', '#33006c', '#ffffff'] : random(colors);
-    setColors(colorPalette)
-    
-    // Set background color
+    const response = await fetch(
+      "https://unpkg.com/nice-color-palettes@3.0.0/100.json"
+    );
+    const palettes = await response.json();
+
+    colorPalette = diwakoStyle
+      ? ["#dd8d0e", "#009599", "#33006c", "#ffffff"]
+      : random(palettes);
+
+    setColors(colorPalette);
+
     const bg = tinycolor
       .mix(colorPalette[0], colorPalette[1], 50)
       .desaturate(10)
       .toString();
-  
-    // Make Lighter version
+
     const bgInner = tinycolor(bg).lighten(10).toString();
-    // And darker version
     const bgOuter = tinycolor(bg).darken(10).toString();
-  
-    // Set to CSS Custom Properties
+
     gsap.to(".container", {
       "--bg-inner": bgInner,
       "--bg-outer": bgOuter,
-      duration: 0.5
+      duration: 0.5,
     });
-  
-    // Set Variables
+
     squareSize = 100;
     numRows = random(4, 8, true);
     numCols = random(4, 8, true);
-  
-    // Create parent SVG
+
     draw = SVG()
       .addTo(".container")
       .size("100%", "100%")
       .viewbox(`0 0 ${numRows * squareSize} ${numCols * squareSize}`)
       .opacity(0);
-  
-    // Create Grid
+
     for (let i = 0; i < numRows; i++) {
       for (let j = 0; j < numCols; j++) {
         generateLittleBlock(i, j, diwakoStyle);
       }
     }
-  
+
     generateBigBlock(diwakoStyle);
-  
+
     gsap.fromTo(
       ".container > svg",
       { opacity: 0, scale: 0.8 },
@@ -178,58 +188,138 @@ const Home: NextPage = () => {
     );
   }
 
+  // Download SVG
+  const downloadSVG = () => {
+    if (!svgContainerRef.current) return;
+    const svgElement = svgContainerRef.current.querySelector("svg");
+    if (!svgElement) return;
+
+    const svgData = new XMLSerializer().serializeToString(svgElement);
+    const svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    const url = URL.createObjectURL(svgBlob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "grid-art.svg";
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+
+  // Download PNG
+  const downloadPNG = () => {
+    if (!svgContainerRef.current) return;
+
+    toPng(svgContainerRef.current)
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.href = dataUrl;
+        link.download = "grid-art.png";
+        link.click();
+      })
+      .catch((err) => {
+        console.error("Error downloading PNG", err);
+      });
+  };
+
   return (
     <div className="wrapper">
       <Head>
-        <title>Creating Generative SVG Grids</title>
-        <meta name="description" content="Inspired from [Creating Generative SVG Grids](https://frontend.horse/articles/generative-grids/) by Alex Trost" />
+        <title>Grid Art Designer</title>
+        <meta
+          name="description"
+          content="Inspired from [Creating Generative SVG Grids](https://frontend.horse/articles/generative-grids/) by Alex Trost"
+        />
         <link rel="icon" href="./favicon.ico" />
         <link rel="manifest" href="./manifest.json" />
       </Head>
-      <AppBar position="static" sx={{backgroundColor: '#dd8d0e'}}>
+      <AppBar position="static" sx={{ backgroundColor: "#dd8d0e" }}>
         <Toolbar>
           <Typography>
             <img
               src="./icon.svg"
-              alt={'Logo Digitalwarenkombinat!'}
+              alt={"Logo Digitalwarenkombinat!"}
               width={300}
               style={{
-                width: '20vw',
-                height: 'auto',
-                maxHeight: '100px',
+                width: "20vw",
+                height: "auto",
+                maxHeight: "100px",
               }}
             />
           </Typography>
-          <Typography variant="h2" component="h1" color="white" sx={{flexGrow: 1, ml: 4}}>SVG Grid</Typography>
+          <Typography
+            variant="h2"
+            component="h1"
+            color="white"
+            sx={{ flexGrow: 1, ml: 4 }}
+          >
+            Grid Art Designer
+          </Typography>
         </Toolbar>
       </AppBar>
-      <Button variant="contained" size="large" sx={{color: '#fff', backgroundColor: '#dd8d0e'}} onClick={generateNewGrid}>Generate your own grid</Button>
-      {init ? <><Container sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
-        <Stack direction="row" spacing={1} sx={{justifyContent: 'center'}}>
-          {colors.map((color, index) => (
-            <Chip key={index} label={color} sx={{backgroundColor: color, color: (theme) => theme.palette.getContrastText(color||'#fff')}}/>
-          ))}
-        </Stack>
-        <FormGroup sx={{alignItems: 'center'}}>
-          <FormControlLabel control={<Switch color="warning" checked={diwakoStyle}
-            onChange={() => setDiwakoStyle(!diwakoStyle)}
-            inputProps={{ 'aria-label': 'controlled' }}/>} label="Set Digitalwarenkombinat style" />
-        </FormGroup>
-        </Container>
-        <div className="container"/>
-       </> :
-      <img
-        src="./about.svg"
-        alt={'Logo Digitalwarenkombinat'}
-        style={{
-          width: '100vw',
-          height: 'auto',
-          maxHeight: '800px'
-        }}
-      />
-      }      
+      <Button
+        variant="contained"
+        size="large"
+        sx={{ color: "#fff", backgroundColor: "#dd8d0e" }}
+        onClick={generateNewGrid}
+      >
+        Generate your own grid
+      </Button>
+      {init ? (
+        <>
+          <Container
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <Stack direction="row" spacing={1} sx={{ justifyContent: "center" }}>
+              {colors.map((color, index) => (
+                <Chip
+                  key={index}
+                  label={color}
+                  sx={{
+                    backgroundColor: color,
+                    color: (theme) =>
+                      theme.palette.getContrastText(color || "#fff"),
+                  }}
+                />
+              ))}
+            </Stack>
+            <FormGroup sx={{ alignItems: "center" }}>
+              <FormControlLabel
+                control={
+                  <Switch
+                    color="warning"
+                    checked={diwakoStyle}
+                    onChange={() => setDiwakoStyle(!diwakoStyle)}
+                    inputProps={{ "aria-label": "controlled" }}
+                  />
+                }
+                label="Set Digitalwarenkombinat style"
+              />
+            </FormGroup>
+            <div className="container" ref={svgContainerRef} />
+            <Button onClick={downloadSVG} sx={{ mt: 2 }} variant="outlined">
+              Download SVG
+            </Button>
+            <Button onClick={downloadPNG} sx={{ mt: 1 }} variant="outlined">
+              Download PNG
+            </Button>
+          </Container>
+        </>
+      ) : (
+        <img
+          src="./about.svg"
+          alt={"Logo Digitalwarenkombinat"}
+          style={{
+            width: "100vw",
+            height: "auto",
+            maxHeight: "800px",
+          }}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
